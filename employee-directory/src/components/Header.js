f07d6d56e92faa3070b82dc65822146c321ea81f
marlon20m <marlonmora.ndr@gmail.com>
2020-08-20 21:28:38 -0500

    adding Header.js

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "Header.js";
+import { Nav } from "react-bootstrap";
+
+
+function App() {
+  return (
+      <Nav />
+    <Header />
+  );
+}
+
+export default App;
